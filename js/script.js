5ba15674ba93141964edcdb98f8fc2a170fd5654
Kaitlyn Joy <57242579+kjingersoll@users.noreply.github.com>
2023-09-06 14:31:10 -0500

    added word list and randomizing

--- js/script.js
+++ js/script.js
@@ -5,8 +5,27 @@ const submitButton = document.getElementById("submit");
 
 let word = "countdown";
 let mixedWord = "";
+let possibles = [];
 
-function scrambleWord() {
+async function getPossibles() {
+    const res = await fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
+    const data = await res.text();
+    possibles = data.split("\r\n");
+    getWord(possibles)
+}
+
+function checkLength(value) {
+    return value.length == 8;
+}
+
+function getWord(possibles) {
+    let correctLengthWords = possibles.filter(checkLength);
+    random = Math.floor(Math.random() * correctLengthWords.length);
+    word = correctLengthWords[random];
+    scrambleWord(word);
+}
+
+function scrambleWord(word) {
     let wordArray = word.split("");
     for (let i=wordArray.length - 1; i > 0; i--) {
         const swap = Math.floor(Math.random() * (i + 1));
@@ -20,11 +39,13 @@ function checkMatch() {
     let guess = guessInput.value;
     if (guess == word) {
         messageDisplay.innerText = "Correct!";
+        wordDisplay.innerText = word;
     } else {
         messageDisplay.innerText = "Try Again";
+        guessInput.value = "";
     }
 }
 
-scrambleWord()
+getPossibles()
 
 submitButton.addEventListener("mouseup", checkMatch);
