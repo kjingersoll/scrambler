b898b2c76555fe8c5abf5507f9b87c02fe075a7b
Kaitlyn Joy <57242579+kjingersoll@users.noreply.github.com>
2023-10-25 13:50:14 -0500

    hide gameplay until difficulty selection

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ const guessInput = document.getElementById("guess");
 const submitButton = document.getElementById("submit");
 const playAgainButton = document.getElementById("play-again");
 const difficultySelect = document.getElementById("difficult-select");
+const playArea = document.getElementById("gameplay");
 
 let word = "countdown";
 let mixedWord = "";
@@ -20,98 +21,103 @@ let remainingGuesses = 10;
 let difficulty = "easy";
 
 function setDifficulty(e) {
-    if(e.target.id == "easy") {
-        difficulty = "easy";
-        totalGuesses = 10;
-        ticketFront.style.backgroundImage = "url('../img/ticket-blue.svg')";
-        ticketBack.style.backgroundImage = "url('../img/ticket-blue.svg')";
-    } else if(e.target.id == "medium") {
-        difficulty = "medium";
-        totalGuesses = 5;
-        ticketFront.style.backgroundImage = "url('../img/ticket.svg')";
-        ticketBack.style.backgroundImage = "url('../img/ticket.svg')";
-    } else if(e.target.id == "hard") {
-        difficulty = "hard";
-        totalGuesses = 3;
-        ticketFront.style.backgroundImage = "url('../img/ticket-red.svg')";
-        ticketBack.style.backgroundImage = "url('../img/ticket-red.svg')";
-    };
-    getWord();
+  if (e.target.id == "easy") {
+    difficulty = "easy";
+    totalGuesses = 10;
+    ticketFront.style.backgroundImage = "url('../img/ticket-blue.svg')";
+    ticketBack.style.backgroundImage = "url('../img/ticket-blue.svg')";
+  } else if (e.target.id == "medium") {
+    difficulty = "medium";
+    totalGuesses = 5;
+    ticketFront.style.backgroundImage = "url('../img/ticket.svg')";
+    ticketBack.style.backgroundImage = "url('../img/ticket.svg')";
+  } else if (e.target.id == "hard") {
+    difficulty = "hard";
+    totalGuesses = 3;
+    ticketFront.style.backgroundImage = "url('../img/ticket-red.svg')";
+    ticketBack.style.backgroundImage = "url('../img/ticket-red.svg')";
+  }
+  playArea.classList.remove("hide");
+  difficultySelect.classList.add("hide");
+  getWord();
 }
 
 async function getPossibles() {
-    const res = await fetch("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa.txt");
-    const data = await res.text();
-    possibles = data.split("\n");
+  const res = await fetch(
+    "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa.txt"
+  );
+  const data = await res.text();
+  possibles = data.split("\n");
 }
 
 function checkLength(value) {
-    if (difficulty === "easy") {
-        return value.length < 6 && value.length > 2
-    } else if (difficulty === "medium") {
-        return value.length > 5 && value.length < 9
-    } else if (difficulty === "hard") {
-        return value.length > 8 && value.length < 11
-    };
+  if (difficulty === "easy") {
+    return value.length < 6 && value.length > 2;
+  } else if (difficulty === "medium") {
+    return value.length > 5 && value.length < 9;
+  } else if (difficulty === "hard") {
+    return value.length > 8 && value.length < 11;
+  }
 }
 
 function getWord() {
-    let correctLengthWords = possibles.filter(checkLength);
-    random = Math.floor(Math.random() * correctLengthWords.length);
-    word = correctLengthWords[random];
-    scrambleWord(word);
+  let correctLengthWords = possibles.filter(checkLength);
+  random = Math.floor(Math.random() * correctLengthWords.length);
+  word = correctLengthWords[random];
+  scrambleWord(word);
 }
 
 function scrambleWord(word) {
-    do {
+  do {
     let wordArray = word.split("");
-    for (let i=wordArray.length - 1; i > 0; i--) {
-        const swap = Math.floor(Math.random() * (i + 1));
-        [wordArray[i], wordArray[swap]] = [wordArray[swap], wordArray[i]];
+    for (let i = wordArray.length - 1; i > 0; i--) {
+      const swap = Math.floor(Math.random() * (i + 1));
+      [wordArray[i], wordArray[swap]] = [wordArray[swap], wordArray[i]];
     }
-    mixedWord = wordArray.join("");}
-    while (mixedWord === word);
-    scrambleText.innerText = mixedWord.toUpperCase();
-    answerText.innerText = word.toUpperCase();
-    remainingGuesses = totalGuesses;
-    guessCounter.innerText = remainingGuesses;
-    console.log(word);
+    mixedWord = wordArray.join("");
+  } while (mixedWord === word);
+  scrambleText.innerText = mixedWord.toUpperCase();
+  answerText.innerText = word.toUpperCase();
+  remainingGuesses = totalGuesses;
+  guessCounter.innerText = remainingGuesses;
+  console.log(word);
 }
 
 function checkMatch(event) {
-    event.preventDefault();
-    remainingGuesses --;
-    let guess = guessInput.value;
-    if (guess === word) {
-        ticketSides.classList.add("flip");
-        messageDisplay.innerText = "Correct!";
-        playAgainButton.classList.remove("hide");
-        guessForm.classList.add("hide");
-    } else if(remainingGuesses >= 1){
-        messageDisplay.innerText = "Try Again";
-        guessInput.value = "";
-        guessCounter.innerText = remainingGuesses;
-    } else {
-        messageDisplay.innerText = "No More Guesses";
-        playAgainButton.classList.remove("hide");
-        ticketContainer.classList.add("fail");
-        guessForm.classList.add("hide");
-        guessCounter.innerText = remainingGuesses;
-    }
+  event.preventDefault();
+  remainingGuesses--;
+  let guess = guessInput.value;
+  if (guess === word) {
+    ticketSides.classList.add("flip");
+    messageDisplay.innerText = "Correct!";
+    playAgainButton.classList.remove("hide");
+    guessForm.classList.add("hide");
+  } else if (remainingGuesses >= 1) {
+    messageDisplay.innerText = "Try Again";
+    guessInput.value = "";
+    guessCounter.innerText = remainingGuesses;
+  } else {
+    messageDisplay.innerText = "No More Guesses";
+    playAgainButton.classList.remove("hide");
+    ticketContainer.classList.add("fail");
+    guessForm.classList.add("hide");
+    guessCounter.innerText = remainingGuesses;
+  }
 }
 
 function playAgain() {
-    ticketSides.classList.remove("flip");
-    ticketContainer.classList.remove("fail");
-    guessForm.classList.remove("hide");
-    playAgainButton.classList.add("hide");
-    guessInput.value = "";
-    messageDisplay.innerText = "";
-    remainingGuesses = totalGuesses;
-    getWord();
+  ticketSides.classList.remove("flip");
+  ticketContainer.classList.remove("fail");
+  guessForm.classList.remove("hide");
+  playAgainButton.classList.add("hide");
+  guessInput.value = "";
+  messageDisplay.innerText = "";
+  remainingGuesses = totalGuesses;
+  difficultySelect.classList.remove("hide");
+  playArea.classList.add("hide");
 }
 
-getPossibles()
+getPossibles();
 
 difficultySelect.addEventListener("click", setDifficulty);
 guessForm.addEventListener("submit", checkMatch);
